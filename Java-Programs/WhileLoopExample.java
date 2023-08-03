9c3ddb2edde1a35ac018b6e7e98aff45facff0c6
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-03 18:32:45 +0530

    While Loop

---
+++ Java-Programs/WhileLoopExample.java
@@ -0,0 +1,9 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Iteration " + i);
+            i++;
+        }
+    }
+}
