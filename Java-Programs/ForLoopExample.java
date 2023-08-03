cb57dee7f1be0c2e9987493bea8f98abfc95dada
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-03 18:32:05 +0530

    Loops (for loop)

---
+++ Java-Programs/ForLoopExample.java
@@ -0,0 +1,7 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration " + i);
+        }
+    }
+}
