d728d2cd4d1fcf11589f906f315609423c0d05df
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-04 00:20:17 +0530

    Variable Example

---
+++ Java-Programs/VariablesExample.java
@@ -0,0 +1,9 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int num = 10;
+        double decimal = 5.5;
+        char letter = 'A';
+        String text = "Java";
+        System.out.println(num + " " + decimal + " " + letter + " " + text);
+    }
+}
