20e214b5e568213512cddcaa72743a035ec79676
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-03 18:30:43 +0530

    Arithmetic Operations

---
+++ Java-Programs/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
