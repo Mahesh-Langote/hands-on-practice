32291f3b8eab0f5d758ccc4db61988db342ae610
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-04 18:33:35 +0530

    Do-While Loop

---
+++ Java-Programs/DoWhileLoopExample.java
@@ -0,0 +1,9 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Iteration " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
