2fcc417992afa80f4a7649c6df217ea0c14f30b5
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-04 18:35:42 +0530

    Enhanced for Loop (for-each)

---
+++ Java-Programs/EnhancedForLoop.java
@@ -0,0 +1,9 @@
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
