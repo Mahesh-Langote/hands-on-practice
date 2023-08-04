2c06da60efb22635324c66a2499f9ca49a16351b
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-04 18:34:57 +0530

    Arrays

---
+++ Java-Programs/ArraysExample.java
@@ -0,0 +1,9 @@
+public class ArraysExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
---
+++ Java-Programs/SwitchExample.java
@@ -0,0 +1,19 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int day = 3;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
