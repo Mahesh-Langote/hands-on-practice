85134ff37aa98e6322c10b3b9c906552d8de07a6
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-05 18:36:24 +0530

    Methods

---
+++ Java-Programs/MethodExample.java
@@ -0,0 +1,10 @@
+public class MethodExample {
+    public static void main(String[] args) {
+        int result = addNumbers(5, 7);
+        System.out.println("Sum: " + result);
+    }
+
+    static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
