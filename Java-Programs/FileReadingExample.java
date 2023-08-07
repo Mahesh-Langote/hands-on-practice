72ab04872446ad17200586c9bb02388a11123e94
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-07 18:54:31 +0530

    File Handling (Reading)

---
+++ Java-Programs/FileReadingExample.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExample {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("example.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
