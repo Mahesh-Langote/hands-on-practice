cf078d9fc4a84ae71bcb554959978c1486865d78
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-09 19:18:11 +0530

    Thread (Extending Thread class)

---
+++ Java-Programs/ThreadExample.java
@@ -0,0 +1,17 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getId() + " Value " + i);
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+
+        MyThread t2 = new MyThread();
+        t2.start();
+    }
+}
