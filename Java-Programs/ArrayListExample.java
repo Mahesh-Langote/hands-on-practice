a9e74cef7d7b5a0d03357ba6f62fb39a1bef21e9
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-09 19:16:24 +0530

    Collections (ArrayList)

---
+++ Java-Programs/ArrayListExample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+}
