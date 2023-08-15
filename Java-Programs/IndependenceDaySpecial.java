91382f4c9749e1fcb28cb9a1fc837120cf470aa8
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-15 14:52:28 +0530

    Todays Special- I proud Im Indian

---
+++ Java-Programs/IndependenceDaySpecial.java
@@ -0,0 +1,29 @@
+public class IndependenceDaySpecial {
+    public static void main(String[] args) {
+        System.out.println("Happy Independence Day!");
+        System.out.println("Let's celebrate the freedom, unity, and diversity of our nation.");
+        System.out.println("Wishing you a day filled with pride, joy, and patriotic spirit!");
+
+        // Display the Indian national anthem
+        displayNationalAnthem();
+    }
+
+    static void displayNationalAnthem() {
+        System.out.println("\nIndian National Anthem - Jana Gana Mana:");
+
+        // Lyrics of the Indian national anthem
+        System.out.println("Jana Gana Mana Adhinayaka Jaya He,");
+        System.out.println("Bharat Bhagya Vidhata");
+        System.out.println("Punjab Sindh Gujarat Maratha,");
+        System.out.println("Dravida Utkala Banga");
+        System.out.println("Vindhya Himachal Yamuna Ganga,");
+        System.out.println("Uchchala Jaladhi Taranga");
+        System.out.println("Tava Shubha Name Jage,");
+        System.out.println("Tava Shubha Ashish Mage,");
+        System.out.println("Gahe Tava Jaya Gatha.");
+        System.out.println("Jana Gana Mangala Dayaka Jaya He,");
+        System.out.println("Bharat Bhagya Vidhata");
+        System.out.println("Jaya He, Jaya He, Jaya He,");
+        System.out.println("Jaya Jaya Jaya, Jaya He!");
+    }
+}
