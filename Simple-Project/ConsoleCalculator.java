4a1e91755b82bda513c4c0aa7f0b73b0d07054d3
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-17 20:09:41 +0530

    ConsoleCalculator

---
+++ Simple-Project/ConsoleCalculator.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class ConsoleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting Calculator. Goodbye!");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Result: " + subtract(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Result: " + multiply(num1, num2));
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        System.out.println("Result: " + divide(num1, num2));
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printMenu() {
+        System.out.println("\n===== Calculator Menu =====");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        System.out.println("5. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    private static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    private static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    private static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    private static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
