4af5e98e2e37a064e00fb34a21074240b6f80cd4
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-24 14:13:39 +0530

    Factorial Calculator

---
+++ real-life-scenario-program/FactorialCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int result = factorial(num);
+        System.out.println("The factorial of " + num + " is: " + result);
+    }
+
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
