4251f3c97336ce7922c049ef892e85ca11323f59
Mahesh Langote <maheshlangote777@gmail.com>
2023-08-26 14:16:34 +0530

    Sum of Digits

---
+++ real-life-scenario-program/SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int sum = calculateSumOfDigits(num);
+        System.out.println("The sum of digits is: " + sum);
+    }
+
+    static int calculateSumOfDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
